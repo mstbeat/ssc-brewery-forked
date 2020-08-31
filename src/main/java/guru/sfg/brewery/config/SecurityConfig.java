eeb2a2b8c75a08955d18ecb2f70b5433631093c0
mstbeat <mrbrightside82@ymail.com>
2020-08-31 16:34:35 +0900

    update:ch6 password security

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -1,15 +1,25 @@
-package guru.sfg.brewery.config;
+ package guru.sfg.brewery.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import guru.sfg.brewery.security.SfgPasswordEncoderFactories;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return SfgPasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -26,5 +36,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.formLogin().and()
 				.httpBasic();
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("spring")
+			.password("{bcrypt}$2a$10$b8JkTXMDnhvdZ7mB3zXcmO8VPY8d/ko0a5fsqWA6ZfjORjoXanoOe")
+			.roles("ADMIN")
+			.and()
+			.withUser("user")
+			.password("{sha256}a09076e35b872736bf7a19fcf0e7788c47dc9f913685d22d846e9de65afac09b3b21e3afa52d7260")
+			.roles("USER")
+			.and()
+			.withUser("scott")
+			.password("{bcrypt15}$2a$15$RLj6ZtVHYnnukYkT27HmiODtdyd.KUQVdNbWDu/hHzZ3iAdSswYK.")
+			.roles("CUSTOMER");
+	}
+	
+//	@Override
+//	@Bean
+//	protected UserDetailsService userDetailsService() {
+//		UserDetails admin = User.withDefaultPasswordEncoder()
+//				.username("spring")
+//				.password("guru")
+//				.roles("ADMIN")
+//				.build();
+//		
+//		UserDetails user = User.withDefaultPasswordEncoder()
+//				.username("user")
+//				.password("password")
+//				.roles("USER")
+//				.build();
+//		
+//		return new InMemoryUserDetailsManager(admin, user);
+//	}
+	
+	
 
 }
--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -11,6 +11,30 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 class BeerControllerIT extends BaseIT {
 
 	@Test
+	void initCreateionFormWithSpring() throws Exception {
+		mockMvc.perform(get("/beers/new").with(httpBasic("spring", "guru")))
+				.andExpect(status().isOk())
+				.andExpect(view().name("beers/createBeer"))
+				.andExpect(model().attributeExists("beer"));
+	}
+	
+	@Test
+	void initCreateionForm() throws Exception {
+		mockMvc.perform(get("/beers/new").with(httpBasic("user", "password")))
+				.andExpect(status().isOk())
+				.andExpect(view().name("beers/createBeer"))
+				.andExpect(model().attributeExists("beer"));
+	}
+	
+	@Test
+	void initCreateionFormWithScott() throws Exception {
+		mockMvc.perform(get("/beers/new").with(httpBasic("scott", "tiger")))
+				.andExpect(status().isOk())
+				.andExpect(view().name("beers/createBeer"))
+				.andExpect(model().attributeExists("beer"));
+	}
+	
+	@Test
 	void findBeers() throws Exception {
 		mockMvc.perform(get("/beers/find"))
 				.andExpect(status().isOk())
